22f89ec0be7a2a20dfe2a66e30039e1df180eaf9
yuanjun.zou <yuanjun.zou@zkteco.com>
2018-12-13 14:48:51 +0800

    动态路由

--- zuk-zuul-server/src/main/java/com/zuk/zuul/Application.java
+++ zuk-zuul-server/src/main/java/com/zuk/zuul/Application.java
@@ -2,50 +2,16 @@ package com.zuk.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
-import org.springframework.context.annotation.Bean;
-
-import com.netflix.zuul.FilterFileManager;
-import com.netflix.zuul.FilterLoader;
-import com.netflix.zuul.groovy.GroovyCompiler;
-import com.netflix.zuul.groovy.GroovyFileFilter;
-import com.zuk.zuul.configuration.FilterConfiguration;
 
 @EnableZuulProxy
 @EnableEurekaClient
-@EnableConfigurationProperties({ FilterConfiguration.class })
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@Bean
-	@RefreshScope
-	@ConfigurationProperties("zuul")
-	public ZuulProperties zuulProperties() {
-		return new ZuulProperties();
-	}
-
-	@Bean
-	public FilterLoader filterLoader(FilterConfiguration filterConfiguration) {
-		FilterLoader filterLoader = FilterLoader.getInstance();
-		filterLoader.setCompiler(new GroovyCompiler());
-
-		try {
-			FilterFileManager.setFilenameFilter(new GroovyFileFilter());
-			FilterFileManager.init(filterConfiguration.getInterval(), filterConfiguration.getRoot() + "/pre",
-					filterConfiguration.getRoot() + "/post");
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-		return filterLoader;
 	}
 
 }
